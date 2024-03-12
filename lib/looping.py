8a3c8df789491a69c10bb6894f34abf311f4bc59
Xennia Contreras <Xenbydesign@gmail.com>
2024-03-12 11:43:32 -0700

    done

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,27 @@
 #!/usr/bin/env python3
 
+# need a while loop to do count down 
 def happy_new_year():
-    # code goes here!
-    pass
+    countdown = 10 
+    while countdown > 0:
+        print(countdown)
+        countdown -= 1
+    print('Happy New Year!')
 
+# need to  iterating through the list 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    squared_list =[]
+    for num in int_list:
+        squared_list.append(num**2)
+    return squared_list
 
+# loop is needed here 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for i in range(1,101):
+        if i % 3 == 0 and i % 5 == 0:
+            print('FizzBuzz')
+        elif i % 3 == 0:
+            print('Fizz')
+        elif i % 5 == 0:
+            print('Buzz')
+        else: print(i)
